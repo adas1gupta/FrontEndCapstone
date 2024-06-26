2e8dc8721fb2d94bf6eb4e87acbed070ae46df7e
adas1gupta <dasgupta1arjun@gmail.com>
2024-06-26 17:56:51 -0400

    Little Lemon website finished

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ComfirmedBooking = () => {
+    return (
+        <div className="confirm">
+            <h1>Booking has been <span>confirmed!</span></h1>
+        </div>
+    );
+};
+
+export default ComfirmedBooking;
---
+++ src/components/Nav.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import logo from '../images/LittleLemon.jpg'
+
+const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    }
+
+    return  (
+        <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+          <a href="/" className='logo'>
+            <img src={logo} alt = 'logo'/>
+          </a>
+          <div className='menu-icon' onClick={toggleMenu}>
+            <div className='bar'></div>
+            <div className='bar'></div>
+            <div className='bar'></div>
+          </div>
+          <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/reservations">Reservations</a></li>
+            <li><a href="/order">Order Online</a></li>
+            <li><a href="/login">Login</a></li>
+          </ul>
+        </nav>
+      );
+}
+
+export default Nav;
